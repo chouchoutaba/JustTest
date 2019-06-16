c1b7b3a3dfefa251546a45c39a9dafcd46eed502
chouchoutaba <82149203@qq.com>
2019-06-16 13:53:46 +0800

    init

---
+++ src/test/java/com/gupaoedu/AppTest.java
@@ -0,0 +1,33 @@
+package com.gupaoedu;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.lang.System.out;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        out.println(111);
+        Map map = new HashMap();
+        List list = new ArrayList();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
